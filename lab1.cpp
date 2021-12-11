cb76b4c7c99909cae495613b2579ec9087fb8167
GeorgiyBelikov <georgiybelikov@mail.ru>
2021-12-11 17:06:12 +0300

    add lab1

---
+++ lab1.cpp
@@ -0,0 +1,18 @@
+//вариант 2
+
+#include <stdio.h>
+
+int main() {
+    float b;
+    float i;
+
+    printf("вещественное число");
+    scanf("%f", &b);
+    printf("%f", b);
+
+    printf("\n\nцелое число");
+    scanf("%i", &i);
+    printf("%i", i)
+
+    return 0;
+}
